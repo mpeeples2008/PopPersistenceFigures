df4d8a2cc1e98a6a529f7512f266fd9006d74d26
Matthew Peeples <mpeeple@asurite.asu.edu>
2020-09-26 20:54:10 -0700

    update

--- PopulationPlot2.R
+++ PopulationPlot2.R
@@ -1,14 +1,14 @@
 library(ggplot2)
 library(egg)
 
-dat <- read.csv('Rome.csv',header=T)
+dat.rome <- read.csv('Rome.csv',header=T)
 dat.london <- read.csv('London.csv',header=T)
+dat.teo <- read.csv('Teo.csv',header=T)
 
-rome <- ggplot(dat) + 
+rome <- ggplot(dat.rome) + 
   geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
   theme(panel.grid.major.y = element_line('darkgray', size=0.25), panel.grid.minor.y=element_blank(), panel.grid.major.x=element_blank(), panel.grid.minor.x=element_blank(), 
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
-  #theme_dark() +
   scale_x_continuous(name="Year", limits=c(-1000, 2020), breaks=c(-1000,-500,0,500,1000,1500,2000),labels=c("1000 BC", "500 BC", "BC/AD","AD 500", "AD 1000","AD 1500","AD 2000")) +
   scale_y_continuous(name="Population in Millions", limits=c(0, 3000000), breaks=c(0,500000,1000000,1500000,2000000,2500000,3000000), labels=c(0,0.5,1,1.5,2,2.5,3)) +
   annotate("rect", xmin = -753, xmax = -750, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
@@ -28,10 +28,14 @@ london <- ggplot(dat.london) +
   geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
   theme(panel.grid.major.y = element_line('darkgray', size=0.25), panel.grid.minor.y=element_blank(), panel.grid.major.x=element_blank(), panel.grid.minor.x=element_blank(), 
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
-  #theme_dark() +
   scale_x_continuous(name="Year", limits=c(-1000, 2020), breaks=c(-1000,-500,0,500,1000,1500,2000),labels=c("1000 BC", "500 BC", "BC/AD","AD 500", "AD 1000","AD 1500","AD 2000")) +
   scale_y_log10(name="Log Population", limits=c(100,10000000), breaks=c(100,1000,10000,100000,1000000,10000000), labels=c(10^2,10^3,10000,100000,1000000,10000000))
 
+teo <- ggplot(dat.teo) +
+  geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
+  theme(panel.grid.major.y = element_line('darkgray', size=0.25), panel.grid.minor.y=element_blank(), panel.grid.major.x=element_blank(), panel.grid.minor.x=element_blank(), 
+        panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
+  scale_x_continuous(name="Year", limits=c(-1000, 2020), breaks=c(-1000,-500,0,500,1000,1500,2000),labels=c("1000 BC", "500 BC", "BC/AD","AD 500", "AD 1000","AD 1500","AD 2000")) +
+  scale_y_continuous(name="Population in Thousands", limits=c(0,110000), breaks=c(0,25000,50000,75000,100000), labels=c(0,25,50,75,100))
 
-
-ggarrange(rome, london, ncol = 1, nrow = 2, labels=c('Rome','London'))
+ggarrange(rome, teo, london, ncol = 1, nrow = 3, labels=c('Rome','Teo','London'))
