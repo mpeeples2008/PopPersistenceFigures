27a3ee1579c0083cf90443ea88a34248c7e2e803
Matthew Peeples <mpeeple@asurite.asu.edu>
2020-09-29 16:10:12 -0700

    update

--- PopulationPlot_collapse.R
+++ PopulationPlot_collapse.R
@@ -17,18 +17,7 @@ rome <- ggplot(dat.rome) +
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
   scale_x_continuous(name="Year", limits=c(-1000, 2020), breaks=c(-1000,-500,0,500,1000,1500,2000),labels=c("1000 BC", "500 BC", "BC/AD","AD 500", "AD 1000","AD 1500","AD 2000")) +
   scale_y_continuous(name="Population in Millions", limits=c(0, 4000000), breaks=c(0,500000,1000000,1500000,2000000,2500000,3000000,3500000,4000000), labels=c(0,0.5,1,1.5,2,2.5,3,3.5,4)) +
-  annotate("rect", xmin = -753, xmax = -750, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Founding of Rome", x = -730, y =3900000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = -509, xmax = -503, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Roman Republic", x = -483, y =3900000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = -27, xmax = -23, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Roman Empire", x = 3, y =3900000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 359, xmax = 410, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Capitol moves/sacking of Rome", x = 430, y =3900000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 1380, xmax = 1600, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Church funded reconstruction", x = 1620, y =3900000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 1861, xmax = 1880, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Italian reunification", x = 1900, y =3900000, color="orange1", angle=270, hjust=0) 
+  annotate("rect", xmin = 550, xmax = 600, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) 
   
 byzantium <- ggplot(dat.byz1) + 
   geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
@@ -36,24 +25,14 @@ byzantium <- ggplot(dat.byz1) +
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
   scale_x_continuous(name="Year", limits=c(-1000, 1800), breaks=c(-1000,-500,0,500,1000,1500,1800),labels=c("1000 BC", "500 BC", "BC/AD","AD 500", "AD 1000","AD 1500","AD 1800")) +
   scale_y_continuous(name="Population in Thousands", limits=c(0, 710000), breaks=c(0,100000,200000,300000,400000,500000,600000,700000), labels=c(0,100,200,300,400,500,600,700)) +
-  annotate("rect", xmin = -657, xmax = -647, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Founding of Byzantium", x = -627, y =695000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 330, xmax = 340, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Founding of Constantinope", x = 360, y =695000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 1453, xmax = 1463, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Capitol Ottoman Empire", x = 1483, y =695000, color="orange1", angle=270, hjust=0) 
-  
+  annotate("rect", xmin = 650, xmax = 700, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) 
 
 byzantium2 <- ggplot(dat.byz2) + 
   geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
   theme(panel.grid.major.y = element_line('darkgray', size=0.25), panel.grid.minor.y=element_blank(), panel.grid.major.x=element_blank(), panel.grid.minor.x=element_blank(), 
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
   scale_x_continuous(name="Year", limits=c(1800, 2020), breaks=c(1800,2000),labels=c("AD 1800","AD 2000")) +
-  scale_y_continuous(name="Population in Millions", limits=c(0, 10000000), breaks=c(0,2500000,5000000,7500000,10000000), labels=c(0,2.5,5,7.5,10)) +
-  annotate("rect", xmin = 1919, xmax = 1923, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Turkish War of Independance", x = 1943, y =9900000, color="orange1", angle=270, hjust=0) 
-  
-  
+  scale_y_continuous(name="Population in Millions", limits=c(0, 10000000), breaks=c(0,2500000,5000000,7500000,10000000), labels=c(0,2.5,5,7.5,10)) 
 
 london <- ggplot(dat.london1) + 
   geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
@@ -61,27 +40,14 @@ london <- ggplot(dat.london1) +
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
   scale_x_continuous(name="Year", limits=c(-1000, 1500), breaks=c(-1000,-500,0,500,1000,1500),labels=c("1000 BC", "500 BC", "BC/AD","AD 500", "AD 1000","AD 1500")) +
   scale_y_continuous(name="Population in Thousands", limits=c(0,100000), breaks=c(0,25000,50000,75000,100000), labels=c(0,25,50,75,100)) +
-  annotate("rect", xmin = 47, xmax = 57, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Founding of Roman Londinium", x = 77, y =95000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 400, xmax = 410, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Abaondonment of Londinium", x = 430, y =95000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 600, xmax = 610, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Anglo-Saxon Lundenwic", x = 630, y =95000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 1066, xmax = 1076, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Norman London", x = 1096, y =95000, color="orange1", angle=270, hjust=0) 
+  annotate("rect", xmin = 400, xmax = 450, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) 
 
 london2 <- ggplot(dat.london2) + 
   geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
   theme(panel.grid.major.y = element_line('darkgray', size=0.25), panel.grid.minor.y=element_blank(), panel.grid.major.x=element_blank(), panel.grid.minor.x=element_blank(), 
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
   scale_x_continuous(name="Year", limits=c(1500, 2020), breaks=c(1500,2000),labels=c("AD 1500","AD 2000")) +
-  scale_y_continuous(name="Population in Millions", limits=c(100000,10000000), breaks=c(0,2500000,5000000,7500000,10000000), labels=c(0,2.5,5,7.5,10)) +
-  annotate("rect", xmin = 1707, xmax = 1727, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Kingdom of Great Britain", x = 1747, y =9500000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 1801, xmax = 1821, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="United Kingdom", x = 1841, y =9500000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 1922, xmax = 1932, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Irish Free State", x = 1952, y =9500000, color="orange1", angle=270, hjust=0) 
+  scale_y_continuous(name="Population in Millions", limits=c(100000,10000000), breaks=c(0,2500000,5000000,7500000,10000000), labels=c(0,2.5,5,7.5,10)) 
 
 teo <- ggplot(dat.teo) +
   geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
@@ -89,35 +55,22 @@ teo <- ggplot(dat.teo) +
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
   scale_x_continuous(name="Year", limits=c(-1000, 2020), breaks=c(-1000,-500,0,500,1000,1500,2000),labels=c("1000 BC", "500 BC", "BC/AD","AD 500", "AD 1000","AD 1500","AD 2000")) +
   scale_y_continuous(name="Population in Thousands", limits=c(0,110000), breaks=c(0,25000,50000,75000,100000), labels=c(0,25,50,75,100)) +
-  annotate("rect", xmin = -150, xmax = -140, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Founding of Teotihuacan", x = -120, y =110000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 500, xmax = 600, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Depopulation of urban core", x = 620, y =110000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 1150, xmax = 1350, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Early Aztec period", x = 1370, y =110000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 1521, xmax = 1531, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Spanish conquest", x = 1551, y =110000, color="orange1", angle=270, hjust=0) 
-
+  annotate("rect", xmin = 1000, xmax = 1050, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) 
+ 
 mex <- ggplot(dat.mex) +
   geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
   theme(panel.grid.major.y = element_line('darkgray', size=0.25), panel.grid.minor.y=element_blank(), panel.grid.major.x=element_blank(), panel.grid.minor.x=element_blank(), 
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
   scale_x_continuous(name="Year", limits=c(-1000, 1800), breaks=c(-1000,-500,0,500,1000,1500,1800),labels=c("1000 BC", "500 BC", "BC/AD","AD 500", "AD 1000","AD 1500","AD 1800")) +
   scale_y_continuous(name="Population in Thousands", limits=c(0,250000), breaks=c(0,50000,100000,150000,200000,250000), labels=c(0,50,100,150,200,250)) +
-  annotate("rect", xmin = 1324, xmax = 1334, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Founding of Tenochitlan", x = 1354, y =240000, color="orange1", angle=270, hjust=0) +
-  annotate("rect", xmin = 1521, xmax = 1531, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Spanish conquest", x = 1551, y =240000, color="orange1", angle=270, hjust=0) 
-  
+  annotate("rect", xmin = 1600, xmax = 1650, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) 
+
 mex2 <- ggplot(dat.mex) +
   geom_line(aes(x = Year, y = Population), size = 1.5, color='turquoise4') +
   theme(panel.grid.major.y = element_line('darkgray', size=0.25), panel.grid.minor.y=element_blank(), panel.grid.major.x=element_blank(), panel.grid.minor.x=element_blank(), 
         panel.background = element_rect(fill = "#2D2D2D"), axis.line = element_line(color='white', size = 1, linetype = "solid")) +
   scale_x_continuous(name="Year", limits=c(1800, 2021), breaks=c(1800,2000),labels=c("AD 1800","AD 2000")) +
-  scale_y_continuous(name="Population in Millions", limits=c(0,25000000), breaks=c(0,5000000,10000000,15000000,20000000,25000000), labels=c(0,5,10,15,20,25))+
-  annotate("rect", xmin = 1821, xmax = 1831, ymin = -Inf, ymax = Inf, fill = "orange1", alpha = 0.6) +
-  annotate("text", label="Mexican Independance", x = 1851, y =24000000, color="orange1", angle=270, hjust=0) 
-  
+  scale_y_continuous(name="Population in Millions", limits=c(0,25000000), breaks=c(0,5000000,10000000,15000000,20000000,25000000), labels=c(0,5,10,15,20,25))
 
 ggsave(filename="Rome.png", plot = rome, width = 300, height = 90, units = c("mm"), dpi = 300)
 ggsave(filename="Teo.png", plot = teo, width = 300, height = 90, units = c("mm"), dpi = 300)
